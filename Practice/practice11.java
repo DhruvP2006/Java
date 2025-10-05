1cb2ce1b82126459207d2f935017570bc39a462c
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-10-06 00:47:52 +0530

    practice

--- Practice/practice11.java
+++ Practice/practice11.java
@@ -1,15 +1,65 @@
-class mp3player{
-  
+class Pen{
+  String color;
+  String type;
+
+  public void write(){
+    System.out.println("writing something");
+  }
+
+  public void printColor(){
+    System.out.println(this.color);
+  }
 }
 
-interface musicPlayer{
-void play(String songName);
-void pause();
-void stop();
+class Student{
+  String name;
+  int age;
+
+public void printInfo(String name){
+  System.out.println(name);
+}  
+
+public void printInfo(int age){
+  System.out.println(age);
+}  
+
+public void printInfo(String name, int age){
+  System.out.println(name +""+age);
+}  
+
+//   public void printInfo(){
+//     System.out.println(this.name);
+//   System.out.println(this.age);
+//   }
+
+//   Student(Student s2){
+// this.name = s2.name;
+// this.age = s2.age;
+//   }
+
+//    Student(){
+
+//   }
 }
 
 public class practice11{
-  purblic static void main(string[]args){
+  public static void main(String args[]){
+    // Pen pen1 = new Pen();
+    // pen1.color = "blue";
+    // pen1.type = "gel";
+    // pen1.write();
 
+    // Pen pen2 = new Pen();
+    // pen2.color = "black";
+    // pen2.type = "ballpoint";
+
+    // pen1.printColor();
+    // pen2.printColor();
+
+    Student s1 = new Student();
+    s1.name = "aman";
+    s1.age = 29;
+
+    s1.printInfo(s1.name, s1.age);
   }
 }
---
+++ Practice/practice12.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import bank.bank;
+
+class Shape{
+    public void area(){
+        System.out.println("displays area")
+
+    }
+}
+
+class Triangle extends Shape{
+public void area(float h, float l){
+    System.out.println(0.5*l*h);
+}
+}
+
+class equilateralTriangle extends Triangle{
+    public void area(float h, float l){
+System.out.println(0.5*l*h);
+    }
+}
+
+class Circle extends Shape{
+    public void area(float r){
+        System.out.println(3.14*r*r);
+    }
+}
+
+public class practice12 {
+    public static void main(String[] args){
+bank.Account account1 = new bank.Account();
+account1.name  = "customer1"
+    }
+}
