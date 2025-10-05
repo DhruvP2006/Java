2e6214beec6765ce2d8afb557f093d009be0c065
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-10-05 09:31:41 +0530

    changes

---
+++ Practice/practice11.java
@@ -0,0 +1,15 @@
+class mp3player{
+  
+}
+
+interface musicPlayer{
+void play(String songName);
+void pause();
+void stop();
+}
+
+public class practice11{
+  purblic static void main(string[]args){
+
+  }
+}
